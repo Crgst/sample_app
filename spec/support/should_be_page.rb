87a6c0c1ac4f58f2603b8eb4cf17b6210c4366ea
Crgst <oco.tasogare@gmail.com>
2014-02-24 15:47:23 +0900

    finish user edit, update, index, and destroy actions

--- spec/support/should_be_page.rb
+++ spec/support/should_be_page.rb
@@ -2,7 +2,9 @@
 RSpec::Matchers.define :should_be_profile_page do |user|
   match do |page|
     it { page.should_be_page user.name }
+    it { page.should have_link 'Users', href: users_path }
     it { page.should have_link 'Profile', href: user_path(user) }
+    it { page.should have_link 'Settings', href: edit_user_path(user) }
     it { page.should have_link 'Sign out', href: signout_path }
     it { page.should_not have_link 'Sign in', href: signin_path }
   end
@@ -17,5 +19,12 @@ end
 RSpec::Matchers.define :should_be_signup_page do
   match do |page|
     it { page.should_be_page 'Sign up' }
+  end
+end
+
+RSpec::Matchers.define :should_be_users_page do
+  match do |page|
+    it { page.should have_selector('title', text: 'All users') }
+    it { page.should have_selector('h1', text: 'All users') }
   end
 end
--- spec/support/valid_action.rb
+++ spec/support/valid_action.rb
@@ -7,7 +7,10 @@ def valid_signup()
 end
 
 def valid_signin(user)
+  visit signin_path
   fill_in "Email",    with: user.email
   fill_in "Password", with: user.password
   click_button "Sign in"
+  # Capybaraを使用していない場合にもサインインする。
+  cookies[:remember_token] = user.remember_token
 end
